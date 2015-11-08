a8f97d7f2a2e6c59d5b4f00b3e7ff47a08f5f193
koushikmln <mln02koushik@gmail.com>
2015-11-09 00:48:32 +0530

    Search Implemented

--- PlainTalk.py
+++ PlainTalk.py
@@ -1,47 +1,78 @@
 import socket
 from constants import *
 from utility import *
+import re
+import threading
 
-class PlainTalk:
+class PlainTalk(threading.Thread):
 
-  def __init__(self, hub, port, username, password):
-    # establish connection
-    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.s.connect((hub, port))
-    self.s.settimeout(2.0)
-    self.username = username
-    self.password = password
+    def __init__(self, hub, port, username, password):
+        # establish connection
+        threading.Thread.__init__(self)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((hub, port))
+        self.s.settimeout(2.0)
+        self.username = username
+        self.password = password
+    def run(self):
+        print "Starting DC SERVER TCP Port"
+        self.talk()
+        print "Exiting TCP"
+    def auth(self):
 
-  def auth(self):
-    
-    # first get the lock
-    i = nbrecv(self.s)
-    lock = i[0].split()[1]
-  
-    # send key and validate request
-    send(self.s, '$Supports UserCommand UserIP2|')
-    send(self.s, '$Key '+key(lock)+'|')
-    send(self.s, '$ValidateNick '+self.username+'|')
+        # first get the lock
+        i = nbrecv(self.s)
+        j = i[0].split("|")
+        m  = re.match('\$\w+',j[0])
+        if m:
+            print m.group(0)
+            
+        lock = i[0].split()[1]
 
-    # get the GetPass request (TODO:add an assert here)
-    nbrecv(self.s)
+        # send key and validate request
+        send(self.s, '$Supports UserCommand UserIP2|')
+        send(self.s, '$Key '+key(lock)+'|')
+        send(self.s, '$ValidateNick '+self.username+'|')
 
-    # send the password
-    send(self.s, '$MyPass '+self.password+'|')
-    
-    # get Hello
-    nbrecv(self.s)
+        # get the GetPass request (TODO:add an assert here)
+        nbrecv(self.s)
 
-    # send some info about yourself
-    send(self.s, '$Version 1,0091|')
-    send(self.s, '$GetNickList|')
-    send(self.s, '$MyINFO $ALL '+self.username+' $$$$$|')
+        # send the password
+        send(self.s, '$MyPass '+self.password+'|')
 
-    # get other stuff
-    nbrecv(self.s)
+        # get Hello
+        nbrecv(self.s)
 
-  def talk(self):
-    self.auth()
-    while True:
-      isend(self.s)
-      nbrecv(self.s)
+        # send some info about yourself
+        send(self.s, '$Version 1,0091|')
+        send(self.s, '$GetNickList|')
+        send(self.s, '$MyINFO $ALL '+self.username+' $$$$$|')
+
+        # get other stuff
+        nbrecv(self.s)
+
+    def talk(self):
+        self.auth()
+        while True:
+          isend(self.s)
+          nbrecv(self.s)
+
+class PlainUDP(threading.Thread):
+    """docstring for PlainUDP"""
+    def __init__(self, ip, port):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+    def run(self):
+        print "Listening on Port"+str(self.ip)+":"+str(self.port)
+        self.listen()
+
+    def listen(self):
+        UDP_IP = self.ip
+        UDP_PORT = self.port
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        sock.bind((UDP_IP, UDP_PORT))
+        while True:
+            data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+            print "received message:", data
+        
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from PlainTalk import *
 
 def main():
 	pt = PlainTalk("172.16.32.222",411 , "testBoy", "12345")
-	pt.talk()
+	pt.start()
+	pu = PlainUDP("192.168.106.203",5000)
+	pu.start()
 
 if __name__ == '__main__':
 	main()
